b994f04cc93a4c939e8bfde34926c42cc8edd243
KimYuRa1 <ura3118@naver.com>
2021-10-29 13:54:50 +0900

    211029

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,43 @@ import React from 'react'
 import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        axios.post('/api/product/products')
+        .then(response => {
+            if(response.data.success){
+                setProducts(response.data.productInfo)
+
+            }else{
+                alert("상품들을 가져오는데 실패했습니다.")
+            }
+        })
+
+    }, [] )
+
+
+    const RenderCards = Products.map((product, index)=>{
+        console.log('product', product)
+
+        return <card >
+            <
+        </card>
+    })
+
+    const renderCards = Products && Products.map( product => (
+
+        <div>
+           <Card>
+            <Card.Meta />
+            </Card> 
+        </div>
+
+    ))
+
+
     return (
         <>
             <div className="app">
